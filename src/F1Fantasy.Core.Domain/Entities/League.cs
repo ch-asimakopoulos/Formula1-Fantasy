e49159bafc376c8e6cc0cd5e383d8ba519166033
ch-asimakopoulos <ch.asimakopoulos@gmail.com>
2020-12-14 00:09:20 +0200

    initial commit for VP to pull and get acquainted with GH

---
+++ src/F1Fantasy.Core.Domain/Entities/League.cs
@@ -0,0 +1,40 @@
+﻿using F1Fantasy.SharedKernel.BaseModel;
+using System;
+
+namespace F1Fantasy.Core.Domain
+{
+    /// <summary>
+    /// A class that contains information regarding a Formula 1 Fantasy League.
+    /// </summary>
+    public class League : TrackingChangesEntity
+    {
+        /// <summary>
+        /// The league's unique identifier.
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// The league's name.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The league's logo.
+        /// </summary>
+        public string Logo { get; set; }
+        /// <summary>
+        /// The league administrator's unique identifier.
+        /// </summary>
+        public string AdminId { get; set; }
+        /// <summary>
+        /// The league leading team's unique identifier.
+        /// </summary>
+        public long LeadingTeamId { get; set; }
+
+
+        //public ICollection<Team> Teams { get; set; }
+
+
+        //public virtual Team LeadingTeam { get; set; }
+        //public virtual ApplicationUser AdminUser { get; set; }
+
+    }
+}
---
+++ src/F1Fantasy.SharedKernel/BaseModel/TrackingChangesEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace F1Fantasy.SharedKernel.BaseModel
+{
+    /// <summary>
+    /// An abstract class that contains information on the creation and the modification of the entity that inherits it.
+    /// </summary>
+    public abstract class TrackingChangesEntity
+    {
+        /// <summary>
+        /// The date and time this entity was created.
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// The user who created this entity.
+        /// </summary>
+        public string CreatedBy { get; set; }
+
+        /// <summary>
+        /// The date and time this entity was last modified.
+        /// </summary>
+        public DateTime DateLastModified { get; set; }
+
+        /// <summary>
+        /// The user who modified this entity last.
+        /// </summary>
+        public string LastModifiedBy { get; set; }
+    }
+}
